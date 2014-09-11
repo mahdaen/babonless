e8fcde22da631f6633be3664e5506fd715a39514
mahdaen <nanang@stucel.com>
2014-09-11 23:15:33 +0700

    Initial setup

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            build: {
+                files: {
+                    'babonless.js': [
+                        'src/_vars.less',
+
+                        'src/mixins/_common.less',
+                        'src/mixins/_css3.less',
+
+                        'src/mixins/_animation.less',
+                        'src/mixins/_border.less',
+                        'src/mixins/_cheats.less',
+                        'src/mixins/_display.less',
+                        'src/mixins/_fonts.less',
+                        'src/mixins/_gradients.less',
+                        'src/mixins/_grids.less',
+                        'src/mixins/_position.less',
+                        'src/mixins/_shadow.less',
+                        'src/mixins/_shape.less',
+                        'src/mixins/_transform.less',
+
+                        'src/mixins/_normalize.less'
+                    ]
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('default', ['concat']);
+}
